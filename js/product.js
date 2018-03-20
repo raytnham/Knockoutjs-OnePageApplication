5ac08f40f89332242477b05fdcd3a93df9df02a1
Ray Nham <ray@raydeveloperonline.com>
2018-03-20 17:44:45 +1100

    initial commit

---
+++ js/product.js
@@ -0,0 +1,50 @@
+var data = [
+    {
+        "ID": "1",
+        "productName": "Bennett",
+        "companyName": "PORTICA",
+        "productPrice": "$1,459.38",
+        "addedDate": "2014-01-31T01:12:41 -11:00",
+        "isActive": true
+    },
+    {
+        "ID": "2",
+        "productName": "Jewel",
+        "companyName": "GEEKMOSIS",
+        "productPrice": "$1,985.29",
+        "addedDate": "2016-05-30T10:06:43 -10:00",
+        "isActive": true
+    },
+    {
+        "ID": "3",
+        "productName": "Lula",
+        "companyName": "ZENOLUX",
+        "productPrice": "$1,580.13",
+        "addedDate": "2015-08-18T04:19:30 -10:00",
+        "isActive": true
+    },
+    {
+        "ID": "4",
+        "productName": "Greer",
+        "companyName": "NETUR",
+        "productPrice": "$2,702.23",
+        "addedDate": "2016-02-26T02:06:12 -11:00",
+        "isActive": true
+    },
+    {
+        "ID": "5",
+        "productName": "Pitts",
+        "companyName": "FROLIX",
+        "productPrice": "$3,555.55",
+        "addedDate": "2015-02-05T07:08:49 -11:00",
+        "isActive": true
+    },
+    {
+        "ID": "6",
+        "productName": "Jenifer",
+        "companyName": "OBONES",
+        "productPrice": "$2,341.25",
+        "addedDate": "2016-09-26T09:20:43 -10:00",
+        "isActive": true
+    }
+]
