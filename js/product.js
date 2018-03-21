fbfa6ab9314bdec267e29c77b7d19a25bd8e84b9
Ray Nham <ray@raydeveloperonline.com>
2018-03-21 11:13:17 +1100

    added view, delete and update functions

--- js/product.js
+++ js/product.js
@@ -1,50 +1,50 @@
 var data = [
     {
-        "ID": "1",
-        "productName": "Bennett",
-        "companyName": "PORTICA",
-        "productPrice": "$1,459.38",
-        "addedDate": "2014-01-31T01:12:41 -11:00",
-        "isActive": true
+      "ID": 0,
+      "productName": "Teasdale",
+      "companyName": "INCUBUS",
+      "productPrice": "$1,366.50",
+      "addedDate": "23-08-2017",
+      "isActive": true
     },
     {
-        "ID": "2",
-        "productName": "Jewel",
-        "companyName": "GEEKMOSIS",
-        "productPrice": "$1,985.29",
-        "addedDate": "2016-05-30T10:06:43 -10:00",
-        "isActive": true
+      "ID": 1,
+      "productName": "Abiquiu",
+      "companyName": "COMBOGEN",
+      "productPrice": "$3,485.81",
+      "addedDate": "28-10-2014",
+      "isActive": true
     },
     {
-        "ID": "3",
-        "productName": "Lula",
-        "companyName": "ZENOLUX",
-        "productPrice": "$1,580.13",
-        "addedDate": "2015-08-18T04:19:30 -10:00",
-        "isActive": true
+      "ID": 2,
+      "productName": "Sunriver",
+      "companyName": "ZAGGLES",
+      "productPrice": "$2,568.65",
+      "addedDate": "05-04-2015",
+      "isActive": true
     },
     {
-        "ID": "4",
-        "productName": "Greer",
-        "companyName": "NETUR",
-        "productPrice": "$2,702.23",
-        "addedDate": "2016-02-26T02:06:12 -11:00",
-        "isActive": true
+      "ID": 3,
+      "productName": "Tuttle",
+      "companyName": "ISONUS",
+      "productPrice": "$400.10",
+      "addedDate": "17-12-2017",
+      "isActive": true
     },
     {
-        "ID": "5",
-        "productName": "Pitts",
-        "companyName": "FROLIX",
-        "productPrice": "$3,555.55",
-        "addedDate": "2015-02-05T07:08:49 -11:00",
-        "isActive": true
+      "ID": 4,
+      "productName": "Soudan",
+      "companyName": "STRALOY",
+      "productPrice": "$281.27",
+      "addedDate": "30-07-2016",
+      "isActive": true
     },
     {
-        "ID": "6",
-        "productName": "Jenifer",
-        "companyName": "OBONES",
-        "productPrice": "$2,341.25",
-        "addedDate": "2016-09-26T09:20:43 -10:00",
-        "isActive": true
+      "ID": 5,
+      "productName": "Nettie",
+      "companyName": "OVERPLEX",
+      "productPrice": "$485.40",
+      "addedDate": "15-01-2018",
+      "isActive": true
     }
-]
+  ]
